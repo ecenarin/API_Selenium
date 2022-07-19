0f6404d0438a06c4696007a863087b5a490864a5
EceNarin <la.crymosa321@gmail.com>
2022-07-19 22:26:33 +0300

    add

---
+++ src/test/java/pojo/ExpDataRestApi.java
@@ -0,0 +1,80 @@
+package pojo;
+
+import javax.annotation.processing.Generated;
+
+@Generated("jsonschema2pojo")
+public class ExpDataRestApi {
+
+    private String status;
+    private DataRestApi data;
+    private String message;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public ExpDataRestApi() {
+    }
+
+    /**
+     *
+     * @param data
+     * @param message
+     * @param status
+     */
+    public ExpDataRestApi(String status, DataRestApi data, String message) {
+        super();
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public DataRestApi getData() {
+        return data;
+    }
+
+    public void setData(DataRestApi data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ExpDataRestApi.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("status");
+        sb.append('=');
+        sb.append(((this.status == null)?"<null>":this.status));
+        sb.append(',');
+        sb.append("data");
+        sb.append('=');
+        sb.append(((this.data == null)?"<null>":this.data));
+        sb.append(',');
+        sb.append("message");
+        sb.append('=');
+        sb.append(((this.message == null)?"<null>":this.message));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
---
+++ src/test/java/tests_api/C29_GetPojoData.java
@@ -0,0 +1,27 @@
+package tests_api;
+
+import org.junit.Test;
+
+public class C29_GetPojoData {
+    @Test
+    public void name() {
+        /*
+                            http://dummy.restapiexample.com/api/v1/employee/3 url’ine
+                            bir GET request gonderdigimizde donen response’un asagidaki gibi oldugunu test edin.
+
+                        	Response Body // expected data
+                                {
+                                "status":"success",
+                                "data":{
+                                    "id":3,
+                                    "employee_name":"Ashton Cox",
+                                    "employee_salary":86000,
+                                    "employee_age":66,
+                                    "profile_image":""
+                                    },
+                                "message":"Successfully! Record has been fetched."
+                                }
+
+                             */
+    }
+}
